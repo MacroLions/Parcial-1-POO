af187a20d2c05f1fc42706a89d7a8e4763239499
Nuria Rivas <00041517@uca.edu.sv>
2018-05-01 11:59:36 -0300

    Se cagregaron algunos atributos y metodos a la clase TarjetaDeCredito

--- src/hotel/rafinha/TarjetaDeCredito.java
+++ src/hotel/rafinha/TarjetaDeCredito.java
@@ -10,5 +10,51 @@ package hotel.rafinha;
  * @author Nuria
  */
 public class TarjetaDeCredito {
+    private String titular;
+    private String bancoAfiliado;
+    private String tipoDeCuenta;
+
+    public TarjetaDeCredito() {
+    }
+
+    public TarjetaDeCredito(String titular, String bancoAfiliado, String tipoDeCuenta) {
+        this.titular = titular;
+        this.bancoAfiliado = bancoAfiliado;
+        this.tipoDeCuenta = tipoDeCuenta;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getBancoAfiliado() {
+        return bancoAfiliado;
+    }
+
+    public void setBancoAfiliado(String bancoAfiliado) {
+        this.bancoAfiliado = bancoAfiliado;
+    }
+
+    public String getTipoDeCuenta() {
+        return tipoDeCuenta;
+    }
+
+    public void setTipoDeCuenta(String tipoDeCuenta) {
+        this.tipoDeCuenta = tipoDeCuenta;
+    }
+
+    public int[] getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int[] codigo) {
+        this.codigo = codigo;
+    }
+    
+    int [] codigo= new int[8]; //Arreglo para el codigo de la tarjeta
     
 }
