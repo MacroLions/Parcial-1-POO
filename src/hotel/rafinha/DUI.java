22264f28be0d8000cf3cad50506ebcdfeb7b0eab
Nuria Rivas <00041517@uca.edu.sv>
2018-05-01 11:59:58 -0300

    Se cagregaron algunos atributos y metodos a la clase DUI

--- src/hotel/rafinha/DUI.java
+++ src/hotel/rafinha/DUI.java
@@ -10,5 +10,60 @@ package hotel.rafinha;
  * @author Nuria
  */
 public class DUI {
+    private String nombre;
+    private String direccion;
+    private String estadoCivil;
+    private String genero;
+
+    public DUI() {
+    }
+
+    public DUI(String nombre, String direccion, String estadoCivil, String genero) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.estadoCivil = estadoCivil;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int[] getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int[] numero) {
+        this.numero = numero;
+    }
     
+    int [] numero= new int[10];//Arreglo para el numero unico del DUI
 }
