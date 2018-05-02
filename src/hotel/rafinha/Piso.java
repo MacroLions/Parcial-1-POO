50589f9edbb4805c48f01d6786778e46f5c8b448
MacroLions <37413420+MacroLions@users.noreply.github.com>
2018-05-02 13:54:48 -0600

    Clase piso agregada.

---
+++ src/hotel/rafinha/Piso.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.rafinha;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maishi
+ */
+public class Piso {
+    private String identificador;
+    private boolean disponibilidad;
+    private ArrayList<Habitacion> habitaciones = new ArrayList();
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public ArrayList<Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(ArrayList<Habitacion> habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+    
+    public void agregarHabitaciones(Habitacion habitacion){
+        this.habitaciones.add(habitacion);
+    }
+    
+    
+    
+    
+}
