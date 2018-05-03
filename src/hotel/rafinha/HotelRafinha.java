bc82872c62e6e6d1d81dd497548b3ca8c3d20dfd
Diana <Diana@DIANA>
2018-05-03 10:47:05 -0600

    Solo modifiqué el nombre de la variable para hacer una prueba. Funciona bien.

--- src/hotel/rafinha/HotelRafinha.java
+++ src/hotel/rafinha/HotelRafinha.java
@@ -5,6 +5,8 @@
  */
 package hotel.rafinha;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Maishi
@@ -15,7 +17,8 @@ public class HotelRafinha {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        
     }
     
-}
+} 
