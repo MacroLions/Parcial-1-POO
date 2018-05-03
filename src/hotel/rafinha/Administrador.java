8ca321c797efb39182ddae73634d0d3e10a80393
Diana <Diana@192.168.0.24>
2018-05-02 19:51:01 -0600

    Aquí está Clase administrador.

---
+++ src/hotel/rafinha/Administrador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.rafinha;
+
+/*
+ *
+ * @author Diana
+ */
+public class Administrador {
+   Reserva[] reservasHechas =  new Reserva[10];
+   Piso[] reservaPiso = new Piso[10];
+   Habitacion[] habiotacionesReservadas = new Habitacion[10];
+   
+}
