7ac402f599dd98f701de25f27cb08aeb7538f401
RichardF12 <37163455+RichardF12@users.noreply.github.com>
2018-05-03 11:26:00 -0600

    Revert "Removiendo el nombre del banco tarjeta"
    This reverts commit 5fa4284a73413b04a1bc70f1d8aa812e7669c4a6.

--- src/hotel/rafinha/Huesped.java
+++ src/hotel/rafinha/Huesped.java
@@ -13,16 +13,18 @@ public class Huesped {
     private String nombre;
     private String numeroDui;
     private String numeroTarjetaCredito;
+    private String bancoTarjeta;
     private Reserva reserva1;
     private Reserva reserva2;
 
     public Huesped() {
     }
     
-    public Huesped(String nombre, String numeroDui, String numeroTarjetaCredito) {
+    public Huesped(String nombre, String numeroDui, String numeroTarjetaCredito, String bancoTarjeta) {
         this.nombre = nombre;
         this.numeroDui = numeroDui;
         this.numeroTarjetaCredito = numeroTarjetaCredito;
+        this.bancoTarjeta = bancoTarjeta;
     }
 
     public Reserva getReserva1() {
@@ -63,6 +65,14 @@ public class Huesped {
 
     public void setNumeroTarjetaCredito(String numeroTarjetaCredito) {
         this.numeroTarjetaCredito = numeroTarjetaCredito;
+    }
+
+    public String getBancoTarjeta() {
+        return bancoTarjeta;
+    }
+
+    public void setBancoTarjeta(String bancoTarjeta) {
+        this.bancoTarjeta = bancoTarjeta;
     }
 
 }
