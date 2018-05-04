c7ad13c18de7e21638e7d0d6e23f6e0d0dff9316
'Diana' <'00226217@uca.edu.sv>
2018-05-03 19:15:03 -0600

    Agregue Menu principal

--- src/hotel/rafinha/HotelRafinha.java
+++ src/hotel/rafinha/HotelRafinha.java
@@ -21,22 +21,50 @@ public class HotelRafinha {
         System.out.println("SISTEMA DE ADMINISTRACION DEL HOTEL 'VIÑA RAFINHA'");
         System.out.println("Ingrese sus credenciales por favor");
         System.out.print("Usuario: ");
-        String usuario= dato.nextLine().toUpperCase();
+        String usuario = dato.nextLine().toUpperCase();
         System.out.print("Contraseña: ");
-        String contra= dato.nextLine().toUpperCase();
-        
-        while(usuario!="USUARIO" && contra!="CONTRASENIA"){
+        String contra = dato.nextLine().toUpperCase();
+
+        while (usuario != "USUARIO" && contra != "CONTRASENIA") {
             System.out.println("Usuario o contraseña incorrectos, intentelo de nuevo");
             System.out.print("Usuario: ");
-            usuario= dato.nextLine().toUpperCase();
+            usuario = dato.nextLine().toUpperCase();
             System.out.print("Contraseña: ");
-            contra= dato.nextLine().toUpperCase();
+            contra = dato.nextLine().toUpperCase();
             break;
         }
-        
+
         System.out.println("¡¡¡¡¡¡¡BIENVENIDO ADMINISTRADOR DEL HOTEL 'VIÑA RAFINHA'!!!!!!! :D ");
         //Aqui iria el menu y los submenus.... ¡Es tu momento de brillar mango Dianita! ¡Make it shineee! Ref. Victorius cancion inicial
+
+        //MENU
+        System.out.println("** MENU PRINCIPAL **");
         
+        System.out.println("** 1. MENU RESERVACION || 2. MENU ADMINISTRACION ** ");
+        
+        Scanner lector = new Scanner(System.in);
+        int opcion = 0;
+        opcion = lector.nextInt();
+        
+        //aun falta que se agregen los metodos dentro de cada uno
+        switch (opcion) {
+            case 1:
+                System.out.println("** MENU RESERVACION **");
+                System.out.println("1. Agregar Reservacion ");
+                System.out.println("2. Eliminar reservacion ");
+                System.out.println("3. Ver reservacion ");
+                System.out.println("4. Modificar reservacion");
+                System.out.println("5. Verificar Reserva");
+                break;
+            case 2:
+                System.out.println("** MENU ADMINISTRADOR **");
+                System.out.println("1. Agregar Piso ");
+                System.out.println("2. Eliminar Piso ");
+                System.out.println("3. Agregar huesped");
+
+                break;
+        }
+
     }
     
-} 
+}
