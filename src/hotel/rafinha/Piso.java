2cc5db7431f09ef30311e0de7ed5a2d60841ce23
'Diana' <'00226217@uca.edu.sv>
2018-05-07 22:18:18 -0600

    java.doc de clase PISO

--- src/hotel/rafinha/Piso.java
+++ src/hotel/rafinha/Piso.java
@@ -11,45 +11,48 @@ import java.util.ArrayList;
  *
  * @author Maishi
  */
+
+/*
+La clase piso contiene las variables necesarias para poder contruir, habilitar y desabilitar los mismos
+*/
 public class Piso {
-    private char identificador;
-    private boolean disponibilidad;
-    public ArrayList<Habitacion> habitaciones = new ArrayList();
+    private char identificador; //Es único
+    private boolean disponibilidad; //Determina si el piso puede ser utilizado o no
+    public ArrayList<Habitacion> habitaciones = new ArrayList(); //Lista de todas las habitaciones que hay en deteminado piso
     
+    //Constructor personalizado
     public Piso(char identificador){
         this.identificador=identificador;
         this.disponibilidad=true;
     }
-
+/*
+    SETTERS Y GETTERS DE LA CLASE PISO
+    */
+    //get por defecto
     public char getIdentificador() {
         return identificador;
     }
-
+ 
+    //Set personzalido
     public void setIdentificador(char identificador) {
         this.identificador = identificador;
     }
-
+    //get por defecto
     public boolean isDisponibilidad() {
         return disponibilidad;
     }
-
+    //Set personzalido
     public void setDisponibilidad(boolean disponibilidad) {
         this.disponibilidad = disponibilidad;
     }
-
+    //get por defecto
     public ArrayList<Habitacion> getHabitaciones() {
         return habitaciones;
     }
-
+    //Set personzalido
     public void setHabitaciones(ArrayList<Habitacion> habitaciones) {
         this.habitaciones = habitaciones;
-    }
-    
-    public void agregarHabitaciones(Habitacion habitacion){
-        this.habitaciones.add(habitacion);
-    }
-    
-    
+    }  
     
     
 }
