b8257ff9ada832f570bd602963297a8e620b354c
MacroLions <00175617@uca.edu.sv>
2018-05-14 08:50:19 -0600

    Clase Pastel de galleta

---
+++ FabricaPasteles/src/fabricapasteles/GalletaPastel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabricapasteles;
+
+
+public class GalletaPastel implements Cake {
+    @Override   
+    public void Hornearse() {
+        System.out.println("El Pastel de galleta fue horneado.... El hombre de gengibre te mira de mala forma.");
+    }
+;
+
+}
