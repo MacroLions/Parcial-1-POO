9734fd231a40dc3da01c920c7dab54e335c6fda1
MacroLions <00175617@uca.edu.sv>
2018-05-14 08:49:16 -0600

    Cheese cake

---
+++ FabricaPasteles/src/fabricapasteles/Cheesecake.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabricapasteles;
+
+
+public class Cheesecake implements Cake {
+    @Override   
+    public void Hornearse() {
+        System.out.println("El cheesecake fue horneado.");
+    }
+;
+
+}
